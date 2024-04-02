e41d12bbcca6c14f2a9632cce9e1d5e9bad35f03
EunbeenDev <ros4312@naver.com>
2024-04-02 15:59:06 +0900

    initial commit

---
+++ src/test/java/com/example/springadvanced/SpringAdvancedApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.springadvanced;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAdvancedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+
+
+}
