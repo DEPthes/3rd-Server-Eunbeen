21200dd997f19e8364e9574c15b99c53c30fa2b7
EunbeenDev <ros4312@naver.com>
2024-04-09 21:52:42 +0900

    [TEST] New Test Code with JUnit and Mokito

--- src/test/java/com/example/springadvanced/SpringAdvancedApplicationTests.java
+++ src/test/java/com/example/springadvanced/SpringAdvancedApplicationTests.java
@@ -8,17 +8,59 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
-
 @SpringBootTest
 class SpringAdvancedApplicationTests {
 
 	@Test
 	void contextLoads() {
+		// Spring Boot 애플리케이션 컨텍스트 로드 테스트
 	}
 
+	//JUnit
 	@Test
 	public void testFindUserById() {
+		// Given
+		UserRepository userRepository = new TestUserRepository();
+		UserService userService = new UserService(userRepository);
+		Long userId = 1L;
+		User user = new User();
+		user.setId(userId);
+		user.setName("하재훈");
+		user.setEmail("haman@example.com");
+		userRepository.save(user); // 테스트용 UserRepository에 사용자 추가
+
+		// When
+		User foundUser = userService.findUserById(userId);
+
+		// Then
+		Assertions.assertNotNull(foundUser);
+		Assertions.assertEquals(userId, foundUser.getId());
+		Assertions.assertEquals("하재훈", foundUser.getName());
+		Assertions.assertEquals("haman@example.com", foundUser.getEmail());
+	}
+
+	// 테스트용 UserRepository 구현
+	private static abstract class TestUserRepository implements UserRepository {
+		private Map<Long, User> users = new HashMap<>();
+
+		@Override
+		public Optional<User> findById(Long id) {
+			return Optional.ofNullable(users.get(id));
+		}
+
+		@Override
+		public Object save(User user) {
+			users.put(user.getId(), user);
+			return null;
+		}
+	}
+
+	//Mockito
+	@Test
+	public void testFindUserById2() {
 		// Given
 		UserRepository userRepository = Mockito.mock(UserRepository.class);
 		UserService userService = new UserService(userRepository);
@@ -38,7 +80,4 @@ class SpringAdvancedApplicationTests {
 		Assertions.assertEquals("하재훈", foundUser.getName());
 		Assertions.assertEquals("haman@example.com", foundUser.getEmail());
 	}
-
-
-
 }
